dadfb5d71f03127693add11dbf5161afcfb8e546
Justin Wu <jwu373@my.bcit.ca>
2022-09-16 12:15:36 -0700

    add

---
+++ Part-1/Week1/test.js
@@ -0,0 +1,42 @@
+"testing" 
+//camelCase
+var bugsCollected = 4; 
+
+//PascalCase
+var BugsCollected = 4;
+
+//Snake_Case
+var Bugs_Collected = 4;
+
+//camelCase
+
+var electronicsBox = "cpu";
+
+//Boolean
+var proceed = true;
+var i = 0;
+
+while(proceed) { 
+    i++; //i =1;
+    proceed = false;
+}
+
+console.log(i);
+
+
+//Date Type - Object
+/**
+ * 6-4-1 variable grannySmith 
+ * colour green
+ * origin australia
+ * origin 1868
+ * console log out the colour
+ * 
+ */
+
+var grannySmith = {
+    colour: "green",
+    originLocation: "australia",
+    originDate: "1868"};
+
+    console.log(grannySmith);
